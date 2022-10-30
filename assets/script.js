523b993be40324bc89cfd4c28064ed78be66e23e
Belma Besirevic <72704864+belmab@users.noreply.github.com>
2022-10-30 09:24:48 +0100

    init finish

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+const accordions = document.querySelectorAll('.accordion')
+
+for (let i = 0; i < accordions.length; i++) {
+  accordions[i].addEventListener('click', function () {
+    //this.classList.toggle("active")
+    const desc = this.nextElementSibling
+    const allDesc = document.querySelectorAll('.desc')
+    const activeAcc = document.getElementsByClassName('accordion active')
+    //console.log(desc);
+    console.log(desc.scrollHeight)
+    if (desc.style.maxHeight) {
+      desc.style.maxHeight = null
+      this.classList.remove('active')
+    } else {
+      for (let i = 0; i < activeAcc.length; i++) {
+        activeAcc[i].classList.remove('active')
+      }
+      for (let i = 0; i < allDesc.length; i++) {
+        allDesc[i].style.maxHeight = null
+      }
+      desc.style.maxHeight = desc.scrollHeight + 'px'
+      this.classList.add('active')
+    }
+  })
+}
